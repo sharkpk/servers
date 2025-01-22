af1e7e6ae8ac7233a84f2a4be6d69c50c06b1f62
sharkpk <155577347+sharkpk@users.noreply.github.com>
2025-01-22 19:12:41 +0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,8 @@ const productRoutes = require("./v1/productRutes");
 const orderRoutes = require("./v1/orderRutes");
 
 router.use("/auth", authRoutes);
-router.use("/customer", customerRoutes);
-router.use("/product", productRoutes);
-router.use("/order", orderRoutes);
+// router.use("/customer", customerRoutes);
+//router.use("/product", productRoutes);
+//router.use("/order", orderRoutes);
 
 module.exports = router;
